be164b7e7ff2ea091db30ec83d372f2dce5f6292
KImberliMartinez <159958568+KImberliMartinez@users.noreply.github.com>
2025-10-29 17:05:24 -0700

    main categorias

---
+++ app/src/main/java/martinez/kimberli/proyectofinal_eq4_recetas/Categorias/Categoria.kt
@@ -0,0 +1,6 @@
+package martinez.kimberli.proyectofinal_eq4_recetas.Categorias
+
+data class Categoria(
+    val nombre: String,
+    val iconRes: Int
+)
--- app/src/main/java/martinez/kimberli/proyectofinal_eq4_recetas/ui/home/HomeFragment.kt
+++ app/src/main/java/martinez/kimberli/proyectofinal_eq4_recetas/ui/home/HomeFragment.kt
@@ -7,9 +7,15 @@ import android.view.ViewGroup
 import android.widget.TextView
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.ViewModelProvider
+import androidx.recyclerview.widget.LinearLayoutManager
+import martinez.kimberli.proyectofinal_eq4_recetas.Categorias.Categoria
+import martinez.kimberli.proyectofinal_eq4_recetas.Categorias.CategoriaAdapter
+import martinez.kimberli.proyectofinal_eq4_recetas.R
 import martinez.kimberli.proyectofinal_eq4_recetas.databinding.FragmentHomeBinding
 
 class HomeFragment : Fragment() {
+
+    private lateinit var categoriaAdapter: CategoriaAdapter
 
     private var _binding: FragmentHomeBinding? = null
 
@@ -28,12 +34,32 @@ class HomeFragment : Fragment() {
         _binding = FragmentHomeBinding.inflate(inflater, container, false)
         val root: View = binding.root
 
+        categoriaAdapter = CategoriaAdapter(categorias) { categoria ->
 
+        }
+
+        binding.categoriasRecycler.apply {
+            adapter = categoriaAdapter
+            layoutManager =
+                LinearLayoutManager(requireContext(), LinearLayoutManager.HORIZONTAL, false)
+        }
         return root
+
+
     }
 
     override fun onDestroyView() {
         super.onDestroyView()
         _binding = null
     }
+
+    private val categorias = listOf(
+        Categoria("Desayuno", R.drawable.ic_desayuno),
+        Categoria("Mexicana", R.drawable.ic_mexicana),
+        Categoria("Asiática", R.drawable.ic_asiatica),
+        Categoria("Postres", R.drawable.ic_postres),
+        Categoria("Mediterránea", R.drawable.ic_mediterranea),
+        Categoria("Americana", R.drawable.ic_americana)
+    )
+
 }
